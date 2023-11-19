c4dbb22779f971620e379cb5361ad9aa2e5cea29
Thana Praserttha <thana.praserttha@gmail.com>
2023-11-20 01:30:16 +0700

    robert

--- src/Pages/Analytics.js
+++ src/Pages/Analytics.js
@@ -3,10 +3,23 @@ import AnalyticsPage from "../Components/AnalyticsPage";
 
 
 export default function Analytics(){
-    return(
+    return (
         <>
-            <NavBar />
-            <AnalyticsPage />
+          <NavBar />
+          <AnalyticsPage />
+          <div>
+            <h1 className="text-2xl font-bold mt-8 ">Welcome To Analytics</h1>
+          </div>
+          
+          <div class= "grid grid-cols-4 grid-rows-4 gap-x-5 gap-y-5">
+            <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2"><div class="h-40 text-white text-center">MAP</div></li>
+            <li class="bg-black rounded-lg shadow-xl"><div class="h-40 text-white text-center">Wind</div ></li>
+            <li class="bg-black rounded-lg shadow-xl"><div class="h-40 text-white text-center">Humid</div></li>
+            <li class="bg-black rounded-lg shadow-xl col-span-2"><div class="h-40 text-white text-center">Sea level </div></li>
+            <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2"><div class="h-20 text-white text-center">Air Pollution</div></li>
+            <li class="bg-black rounded-lg shadow-xl col-span-2"><div class="h-40 text-white text-center">Presure</div></li>
+          </div>
+          
         </>
-    )
+      );
 }
