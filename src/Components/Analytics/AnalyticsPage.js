16d82bdb30b1a46a158e8812a3d120d7efb209f3
Gehan44 <gehan.dub@gmail.com>
2023-11-20 19:52:21 +0700

    AnalyticesPage.js

--- src/Components/Analytics/AnalyticsPage.js
+++ src/Components/Analytics/AnalyticsPage.js
@@ -26,34 +26,37 @@ export default function AnalyticsPage() {
     }, []);
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center lg:px-8">
+        <div className="h-full flex flex-col justify-center ">
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <div className="m-3">
+                <div className="m-3 h-full">
                     {error && <p>Error: {error}</p>}
                     {weekForecast.list.slice(0, 1).map((forecastItem) => (
                         <div key={forecastItem.dt}>
-                            <div class="grid grid-cols-4 grid-rows-4 gap-x-5 gap-y-5">
-                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
+                            <div class="grid grid-cols-4 grid-rows-3 gap-x-5 gap-y-5">
+                                <div class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
                                     <div className="h-40 max-h-full text-white text-center row-span-2">
                                     </div>
-                                </li>
-                                <li class="bg-black rounded-lg shadow-xl">
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl">
                                     <div class="h-40 text-white text-center">Wind Speed: {forecastItem.wind.speed}</div >
-                                </li>
-                                <li class="bg-black rounded-lg shadow-xl">
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl">
                                     <div class="h-40 text-white text-center">Humidity: {forecastItem.main.humidity}</div>
-                                </li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2">
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl">
+                                    <div class="h-40 text-white text-center">Humidity: {forecastItem.main.humidity}</div>
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl col-span-2">
                                     <div class="h-40 text-white text-center">Sea level: {forecastItem.main.sea_level}</div>
-                                </li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl col-span-2">
                                     <div class="h-20 text-white text-center">Air Pollution</div>
-                                </li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2">
+                                </div>
+                                <div class="bg-black rounded-lg shadow-xl col-span-2">
                                     <div class="h-40 text-white text-center">Pressure: {forecastItem.main.pressure}</div>
-                                </li>
+                                </div>
                             </div>      
                         </div>
                     ))}
