7c3bc209cfc06c5825c1e51c0705a9538982339f
Gehan44 <gehan.dub@gmail.com>
2023-11-20 18:50:49 +0700

    AnalyticesPage.js

--- src/Components/Analytics/AnalyticsPage.js
+++ src/Components/Analytics/AnalyticsPage.js
@@ -9,7 +9,6 @@ export default function AnalyticsPage() {
 
     const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -27,33 +26,39 @@ export default function AnalyticsPage() {
     }, []);
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center lg:px-8">
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div className="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
                     {error && <p>Error: {error}</p>}
-                    {weekForecast.list.map((forecastItem) => (
-                        <div key={forecastItem.id === 1}>
-                            <div>
-                                <h1 className="text-2xl font-bold mt-8 ">Welcome To Analytics</h1>
-                            </div>
+                    {weekForecast.list.slice(0, 1).map((forecastItem) => (
+                        <div key={forecastItem.dt}>
                             <div class="grid grid-cols-4 grid-rows-4 gap-x-5 gap-y-5">
-                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2"><div class="h-40 text-white text-center">MAP</div></li>
-                                <li class="bg-black rounded-lg shadow-xl"><div class="h-40 text-white text-center">Wind Speed :{forecastItem.wind.speed}</div ></li>
-                                <li class="bg-black rounded-lg shadow-xl"><div class="h-40 text-white text-center">Humid</div></li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2"><div class="h-40 text-white text-center">Sea level </div></li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2"><div class="h-20 text-white text-center">Air Pollution</div></li>
-                                <li class="bg-black rounded-lg shadow-xl col-span-2"><div class="h-40 text-white text-center">Presure</div></li>
-                            </div>
-                            <hr />
+                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
+                                    <div className="h-40 max-h-full text-white text-center row-span-2">
+                                    </div>
+                                </li>
+                                <li class="bg-black rounded-lg shadow-xl">
+                                    <div class="h-40 text-white text-center">Wind Speed: {forecastItem.wind.speed}</div >
+                                </li>
+                                <li class="bg-black rounded-lg shadow-xl">
+                                    <div class="h-40 text-white text-center">Humidity: {forecastItem.main.humidity}</div>
+                                </li>
+                                <li class="bg-black rounded-lg shadow-xl col-span-2">
+                                    <div class="h-40 text-white text-center">Sea level: {forecastItem.main.sea_level}</div>
+                                </li>
+                                <li class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
+                                    <div class="h-20 text-white text-center">Air Pollution</div>
+                                </li>
+                                <li class="bg-black rounded-lg shadow-xl col-span-2">
+                                    <div class="h-40 text-white text-center">Pressure: {forecastItem.main.pressure}</div>
+                                </li>
+                            </div>      
                         </div>
                     ))}
                 </div>
             )}
-
         </div>
-
-
     );
 }
