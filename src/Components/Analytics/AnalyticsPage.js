bdba29195adddce1d327af7ff99b0b46376e7bb3
Gehan44 <gehan.dub@gmail.com>
2023-11-20 23:58:04 +0700

    AnalyticesPage.js

--- src/Components/Analytics/AnalyticsPage.js
+++ src/Components/Analytics/AnalyticsPage.js
@@ -100,7 +100,14 @@ export default function AnalyticsPage() {
                                         .map((forecastItem) => (
                                             <div className='w-40 h-full bg-white p-2 rounded'>
                                                 <div key={forecastItem.dt} className="" style={{ width: '100px' }}>
-                                                    {forecastItem.main.temp}
+                                                    <img
+                                                        src={`https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`}
+                                                        className="icon-small shadow-md"
+                                                        alt="weather"
+                                                    />
+                                                    <p className='text-orange-300'>{new Date(forecastItem.dt_txt).toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })}</p>
+                                                    <p>{new Date(forecastItem.dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                                                    <p>{forecastItem.main.temp} °C</p>
                                                 </div>
                                             </div>
                                         ))}
