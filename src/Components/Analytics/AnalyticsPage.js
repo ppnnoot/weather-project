fc58daaf5b1cbdf8884e7dc4e8188c59d4f1c4e4
Gehan44 <gehan.dub@gmail.com>
2023-11-20 22:47:30 +0700

    AnalyticesPage.js

--- src/Components/Analytics/AnalyticsPage.js
+++ src/Components/Analytics/AnalyticsPage.js
@@ -1,44 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {fetchWeatherAsync} from "../../API/WeatherSlice";
+import { fetchWeatherAsync } from "../../API/WeatherSlice";
 import { list } from '@material-tailwind/react';
 
 export default function AnalyticsPage() {
     const dispatch = useDispatch();
     const [position, setPosition] = useState(null)
-    const { today, forecast,weekly, status, error } = useSelector((state) => state.weather);
-    const lastsearch = useSelector((state)=> state.weather.lastSearch)
+    const { today, forecast, weekly, status, error } = useSelector((state) => state.weather);
+    const lastsearch = useSelector((state) => state.weather.lastSearch)
 
     useEffect(() => {
-            try {
-                //console.log("lastsearch",lastsearch)
-                if (lastsearch) {
-                    //console.log("dispatch(fetchWeatherAsync(searchData.lat, searchData.lon)")
-                    const pos = { lat: lastsearch.value.lat, lon: lastsearch.value.lon };
+        try {
+            //console.log("lastsearch",lastsearch)
+            if (lastsearch) {
+                //console.log("dispatch(fetchWeatherAsync(searchData.lat, searchData.lon)")
+                const pos = { lat: lastsearch.value.lat, lon: lastsearch.value.lon };
+                dispatch(fetchWeatherAsync(pos));
+            } else {
+                const positionProm = new Promise((posResolve, posReject) => {
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(
+                            (position) => posResolve(position.coords),
+                            (err) => posReject(new Error(err.message))
+                        );
+                    } else {
+                        posReject(new Error('Geolocation not supported'));
+                    }
+                });
+
+                positionProm.then(({ latitude, longitude }) => {
+                    //console.log(latitude, longitude);
+                    const pos = { lat: latitude, lon: longitude };
                     dispatch(fetchWeatherAsync(pos));
-                }else {
-                    const positionProm = new Promise((posResolve, posReject) => {
-                        if (navigator.geolocation) {
-                            navigator.geolocation.getCurrentPosition(
-                                (position) => posResolve(position.coords),
-                                (err) => posReject(new Error(err.message))
-                            );
-                        } else {
-                            posReject(new Error('Geolocation not supported'));
-                        }
-                    });
+                })
+            }
+        } catch (err) {
+            console.error('Error fetching weather data:', err);
+        }
 
-                    positionProm.then(({ latitude, longitude }) => {
-                        //console.log(latitude, longitude);
-                        const pos = { lat: latitude, lon: longitude };
-                        dispatch(fetchWeatherAsync(pos));
-                    })
-                }
-            }catch (err) {
-                    console.error('Error fetching weather data:', err);
-                }
-
-    }, [dispatch,lastsearch]);
+    }, [dispatch, lastsearch]);
     if (status === 'loading') {
         return <p>Loading...</p>;
     }
@@ -53,49 +53,63 @@ export default function AnalyticsPage() {
 
     return (
         <>
-           
-                <div className="m-3 h-full">
-                    {error && <p>Error: {error}</p>}
-                    {weekly.list.slice(0,1).map((forecastItem) => (
-                        <div key={forecastItem.dt}>
-                            
-                            <div class="grid grid-cols-4 grid-rows-3 gap-x-5 gap-y-5">
-                                <div class="bg-black rounded-lg shadow-xl col-span-2 row-span-2">
-                                    <div className="h-40 max-h-full text-white text-center row-span-2">
-                                    </div>
-                                </div>
-                                <div class="bg-black rounded-lg shadow-xl">
-                                    <div class="h-40 text-white text-center">Temp at {forecastItem.dt_txt} <br></br> {forecastItem.main.temp}°C </div >
-                                </div>
-                                <div class="bg-black rounded-lg shadow-xl">
-                                    <div class="h-40 text-white text-center">Temp Min/Max <br></br> {forecastItem.main.temp_min}°C/{forecastItem.main.temp_max}°C</div>
-                                </div>
-                                <div class="bg-black rounded-lg shadow-xl col-span-2">
-                                    <div class="h-40 text-white text-center">Sea level: {forecastItem.main.sea_level}</div>
-                                </div>
-                                {/* <div class="bg-black rounded-lg shadow-xl col-span-2">
-                                    <div class="h-20 text-white text-center">Air Pollution</div>
-                                </div>
-                                <div class="bg-black rounded-lg shadow-xl col-span-2">
-                                    <div class="h-40 text-white text-center">Pressure: {forecastItem.main.pressure}</div>
-                                </div> */}
-                                <div class="bg-black rounded-lg shadow-xl col-span-4 ">
 
-                                    <div class="h-20 text-blue text-center flex flex-onwrap">{weekly.list.slice(0,(forecastItem.count)).map((forecastItem) => (
-                                        <div key={forecastItem.dt}>
-                                            <div class="h-full m-2 bg-white">
-                                                {forecastItem.main.temp}
-                                            </div>
-                                        </div>
-                                    ))}
-
-                                    </div>
-                                </div>
-                            </div>      
-                        </div>
-                    ))}
+<div className="m-3 h-full">
+  {error && <p className="text-red-500">Error: {error}</p>}
+  {weekly.list.slice(0, 1).map((forecastItem) => (
+    <div key={forecastItem.dt}>
+      <div className="grid grid-cols-4 grid-rows-3 gap-5">
+        <div className="bg-gray-800 rounded-lg shadow-xl col-span-2 row-span-2 flex items-center justify-center p-4">
+          <div className="h-40 text-white text-center">
+            {/* You can customize this section based on your design requirements */}
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-xl flex items-center justify-center p-4">
+          <div className="h-40 text-white text-center">
+            Date <br />
+            {forecastItem.dt_txt}
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-xl flex items-center justify-center p-4">
+          <div className="h-40 text-white text-center">
+            Temp: {forecastItem.main.temp}°C <br />
+            Temp Min/Max: <br />
+            {forecastItem.main.temp_min}°C/{forecastItem.main.temp_max}°C
+            <br />
+            Feels Like: {forecastItem.main.feels_like}°C
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-xl col-span-1 flex items-center justify-center p-4">
+          <div className="h-40 text-white text-center">
+            Wind <br />
+            Speed: {forecastItem.wind.speed}<br />
+            Deg: {forecastItem.wind.deg}<br />
+            Gust: {forecastItem.wind.gust}
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-xl col-span-1 flex items-center justify-center p-4">
+          <div className="h-40 text-white text-center">
+            Pressure: {forecastItem.main.pressure} <br />
+            Sea level: {forecastItem.main.sea_level}<br />
+            Humidity: {forecastItem.main.humidity}
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-xl col-span-4 p-4">
+          <div className="h-20 text-blue text-center flex space-x-2 overflow-x-auto">
+            {weekly.list
+              .slice(0, forecastItem.count)
+              .map((forecastItem) => (
+                <div key={forecastItem.dt} className="h-full bg-white p-2 rounded">
+                  {forecastItem.main.temp}
                 </div>
-            
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
+
         </>
     );
 }
