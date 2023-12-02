ef7ac1ae9e251871c67d3109433c42e150f15dfb
Pakpoom Serianut <pakpoom.sria@gmail.com>
2023-12-02 23:01:55 +0700

    marge

--- src/API/AuthAPI.js
+++ src/API/AuthAPI.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
 const CONTROL_API = 'https://localhost:3000/api/login'
 
 
@@ -27,15 +26,4 @@ export function loginUser(loginInfo){
             
         }
     })
-=======
-const CONTROL_API = 'https://dummyjson.com'
-axios.defaults.baseURL = 'http://localhost:3001/'
-export async function loginUser(loginInfo){
-    try {
-        const res = await axios.post('/api/login',loginInfo)
-        return res.data
-    }catch (err){
-
-    }
->>>>>>> da8f27ea531e45f0c91dc5777c2bc652a76e00f8
 }
