12064089fe12158948e04ab66a0304c4f5d4c41b
Pakpoom Serianut <pakpoom.sria@gmail.com>
2023-12-02 21:02:38 +0700

    poom

--- src/API/AuthAPI.js
+++ src/API/AuthAPI.js
@@ -1,24 +1,26 @@
+import axios from "axios";
+const CONTROL_API = 'https://localhost:3000/api/login'
 
-const CONTROL_API = 'https://dummyjson.com'
+
 export function loginUser(loginInfo){
     return new Promise(async (resolve,reject)=>{
         try {
             console.log(loginInfo)
-            const response = await fetch(`https://dummyjson.com/auth/login`,{
+            const response = await fetch(CONTROL_API,{
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    username: loginInfo.email,
+                    username: loginInfo.username,
                     password: loginInfo.password
                 })
             });
             if(response.ok){
                 const data = await response.json()
-                //console.log(data)
+                console.log(data)
                 resolve({data});
             }
             const err = await response.text()
-            reject(err)
+            reject(err) 
         }catch (err) {
             reject(err)
             
